c5f8eddbe7e13490a69827071444549a5219478f
Emre <rmapeta@ucl.ac.uk>
2021-02-04 19:32:13 +0300

    Data generation for RBF and evaluate()

--- part2/RBFSpline.m
+++ part2/RBFSpline.m
@@ -2,35 +2,29 @@
 classdef RBFSpline
 %     properties
 %     end
-    methods
-        function alpha = fit(s_points, t_points, lambda, sigma)
+    methods (Static)
+        
+        function alpha = fit(t_points, lambda, sigma)
             % Inputs
             % s_points = source points
             % t_points = target points (qk)
             % lambda = aprox. localisation error
-            n = size(s_points,1); % Size of source points
+            n = size(t_points,1); % Size of source points
             
             % Construct W
-            m = ones(1,n);
-            w = m./sigma^2;
-            W = diag(w);
+            W = diag(1./sigma.^2);
             
-            % Initialise R
-            R = zeros(1,3);
+            K = RBFSpline.kernel_gaussian(t_points, sigma);
             
-            K = kernel_gaussian(s_points, t_points, sigma);
-            
-            R(1) = K(:,1) + lambda*inv(W);
-            R(2) = K(:,2) + lambda*inv(W);
-            R(3) = K(:,3) + lambda*inv(W);
+            R = K + lambda*inv(W);
      
             % Only finding alpha since beta is absent for Gaussian RBF Kernel
             
-            alpha = inv(R).*t_points(:,1);
+            alpha = inv(R)*t_points;
             
             % Output
             % [alpha beta] = spline coefficients
-            
+        end
         function t_q_points = evaluate(q_points, c_points, alpha, sigma)
             % Inputs
             % q_points = query points that are needed to be transformed
@@ -44,26 +38,26 @@ classdef RBFSpline
             % Output
             %  t_q_points = transformed query points
         end
-        end
+        
         % RBF kernel function
-        function K = kernel_gaussian(q_points, c_points, sigma)
+        function K = kernel_gaussian(c_points, sigma)
             % Inputs
             % q_points = data matrix with training points
-            % c_points = data matrix with test samples
             
-            nq = size(q_points,1);
             nc = size(c_points,1);
             
-            % Sum of the elements, squared
-            nq_sq = sum(q_points.^2,1);
-            nc_sq = sum(c_points.^2,1);
+            % Initialise Kernel
+            K = zeros(nc);
             
-            % Euclidean distances
-            D = (ones(nc,1)*nq_sq)' + ones(nq,1)*nc_sq - 2*q_points'*c_points;
+            for i = 1:nc
+                for j = 1:i-1
+                    D = norm(c_points(i,:)-c_points(j,:));
+                    K(i,j) = exp(-D/(2.*sigma(i)));
+                end
+            end
             
-            % Kernel
-            K = exp(-D/(2*sigma^2));
-            
+            K = K + K' + eye(nc);
+
             % Outputs
             % K = [KX, KY, KZ] = Kernel values between the query points and the control point
             % sets
